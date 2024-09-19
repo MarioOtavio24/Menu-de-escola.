62061f09a00e7ade43f60d8dfbaff00347f3fc18
Mario Otavio Nascimento <otaviomario4@gmail.com>
2024-09-19 17:25:38 -0300

    Add files via upload
    Descobrindo qual a idade dos alunos, para entender quem é mais velho ou da mesma idade para organizar a sala de aula.
    obviamente é só um protótipo

---
+++ Calculando a idade.py
@@ -0,0 +1,9 @@
+Idade1=int(input("digite a idade do primeiro usuario ")) #isto aqui, é apenas um pequeno menu para mostrar qual usuário é mais velho ou mesma idade.
+Idade2=int(input("digite a idade do segundo usuario "))
+if Idade1 > Idade2:
+    print ("o primeiro usuario é mais velho")
+if Idade1 >= Idade2:
+        print ("eles tem a mesma idade")  
+else:
+    Idade1 < Idade2
+    print("o segundo é mais velho")
